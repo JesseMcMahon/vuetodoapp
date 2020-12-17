b43b0b8f4bd3457b9965294940d5c7d6836fe336
Jesse McMahon <Jessemcmahon17@gmail.com>
2020-12-17 12:02:14 -0500

    final push to github before submission

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb+srv://jessemcmahon:Jessiah!1@peerpal.wmzl9.mongodb.net/test",
+    database: "mongodb+srv://jessemcmahon:<Password>@peerpal.wmzl9.mongodb.net/test",
     secret: "password"
   };
